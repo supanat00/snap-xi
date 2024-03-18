36ab6304418a80d378771108341795b6fddc986c
supanat00 <supanat90@gmail.com>
2024-03-18 15:46:33 +0700

    fullscreen-update-1

--- src/pages/SnapCamera/SnapCamera.jsx
+++ src/pages/SnapCamera/SnapCamera.jsx
@@ -102,7 +102,13 @@ const SnapCamera = () => {
         id="canvas-container"
         width={window.innerWidth}
         height={window.innerHeight}
-        style={{ width: "100%", height: "100%" }}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "fixed",
+          top: 0,
+          left: 0,
+        }}
       ></canvas>
     </div>
   );
