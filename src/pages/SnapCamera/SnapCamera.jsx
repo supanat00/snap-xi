b07aaf502f003eb6e5c385d396e349665907216f
supanat00 <supanat90@gmail.com>
2024-03-18 19:29:58 +0700

    update-container-add-01

--- src/pages/SnapCamera/SnapCamera.jsx
+++ src/pages/SnapCamera/SnapCamera.jsx
@@ -46,19 +46,9 @@ const SnapCamera = () => {
 
     mediaStream = await navigator.mediaDevices.getUserMedia({
       video: {
-        width: {
-          min: 1280,
-          ideal: 1920,
-          max: 2560,
-        },
-        height: {
-          min: 720,
-          ideal: 1080,
-          max: 1440,
-        },
-        facingMode: {
-          exact: "environment",
-        },
+        width: { ideal: 1080 },
+        height: { ideal: 1920 },
+        facingMode: "environment",
       },
     });
 
