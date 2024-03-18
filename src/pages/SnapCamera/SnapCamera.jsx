d35a339d13a8504ee88c997391eda5c090c4083a
supanat00 <supanat90@gmail.com>
2024-03-18 19:15:34 +0700

    test-add-ratio-01

--- src/pages/SnapCamera/SnapCamera.jsx
+++ src/pages/SnapCamera/SnapCamera.jsx
@@ -46,16 +46,16 @@ const SnapCamera = () => {
 
     mediaStream = await navigator.mediaDevices.getUserMedia({
       video: {
-        // width: {
-        //   min: 1280,
-        //   ideal: 1920,
-        //   max: 2560,
-        // },
-        // height: {
-        //   min: 720,
-        //   ideal: 1080,
-        //   max: 1440,
-        // },
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440,
+        },
         facingMode: {
           exact: "environment",
         },
