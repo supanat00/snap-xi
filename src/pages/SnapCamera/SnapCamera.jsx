d1609e7f2faeaa6f9a0c818a32ed984f2bd08aeb
supanat00 <supanat90@gmail.com>
2024-03-18 16:14:05 +0700

    test-style-updates-1

--- src/pages/SnapCamera/SnapCamera.jsx
+++ src/pages/SnapCamera/SnapCamera.jsx
@@ -82,7 +82,7 @@ const SnapCamera = () => {
       <canvas
         ref={canvasRef}
         id="canvas-container"
-        style={{ width: "100vh", height: "100vh" }}
+        style={{ width: "100vh", height: "200px" }}
       ></canvas>
     </div>
   );
